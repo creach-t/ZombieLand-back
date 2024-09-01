b21ca341c0a57c9944d03ad79cedb13d08576b3c
CedricMrt <cjyiphone@gmail.com>
2024-09-01 18:00:47 +0200

    feat: :sparkles: add chat fonction and chat admin page

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
-import "dotenv/config";
-import express from "express";
-import session from "express-session";
-import cors from "cors";
-import errorHandler from "./src/middlewares/errorHandler.js";
-import router from "./src/router/index.js";
+import 'dotenv/config';
+import express from 'express';
+import http from 'http';
+import session from 'express-session';
+import cors from 'cors';
+import errorHandler from './src/middlewares/errorHandler.js';
+import router from './src/router/index.js';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-// Configure view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
 
-// Configure assets routes (static folder)
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
 app.use(router);
 
 app.use(errorHandler);
 
+io.on('connection', (socket) => {
+  console.log('Un utilisateur est connecté');
+
+  socket.on('joinAdmin', () => {
+    socket.join('admin');
+    console.log('Admin rejoint');
+  });
+
+  socket.on('joinClient', () => {
+    socket.join('client');
+    console.log('Client rejoint');
+  });
+
+  socket.on('message', (message) => {
+    console.log('Message reçu :', message);
+
+    io.emit('message', message);
+
+    io.to('admin').emit('newMessageNotification', 'Nouveau message reçu');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Un utilisateur est déconnecté');
+  });
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,15 @@ import categoryRouter from './category.js';
 import userRouter from './user.js';
 import loginRouter from './login.js';
 import signinRouter from './signin.js';
+import adminRouter from './admin.js';
 
 const router = Router();
 
 router.use('/activities', activityRouter);
 router.use('/booking', bookingRouter);
 router.use('/account', userRouter);
+router.use('/admin-home', adminRouter);
 /* router.use('/signin', signin);
 router.use('/login', loginRouter); */
 
 export default router;
-
