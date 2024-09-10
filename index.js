13e5c3736509345cb92262af121adcc1a7b163be
CedricMrt <cjyiphone@gmail.com>
2024-09-10 14:40:36 +0200

    fix: :bug: fix chat

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
+let pendingMessages = [];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
